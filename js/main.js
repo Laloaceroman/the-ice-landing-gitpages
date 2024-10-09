39ef203f11ac3de3ec5e8b088079497aafbe675f
Eduardo Acevedo <eduacevedoroman@gmail.com>
2024-10-08 21:36:46 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -286,12 +286,15 @@ app.pagepiling = {
     } else {
       return $('#pagepiling').pagepiling({
         direction: 'vertical',
-        sectionsColor: ['#ffffff', '#EFEFF1', '#ffffff', '#EFEFF1', '#ffffff', '#EFEFF1'],
+        sectionsColor: ['#ffffff', '#ffffff', '#EFEFF1', '#ffffff', '#ffffff', '#ffffff', '#ffffff'],
         navigation: {
           'position': 'right',
-          'tooltips': ['Sección 1', 'Sección 2', 'Sección 3', 'Sección 4', 'Sección 5', 'Sección 6']
+          'tooltips': ['Sección 1', 'Sección 2', 'Sección 3', 'Sección 4', 'Sección 5', 'Sección 6', 'Sección 7']
         },
-        scrollingSpeed: 700
+        scrollingSpeed: 500,
+        afterLoad: function(anchorLink, index) {
+          return $(".section" + index + " .dscroll").addClass('dscroll--in');
+        }
       });
     }
   }
